513862a4aa34d33f6a302c1c0837d3f6970c7227
Kee Jefferys <kee@loki.network>
2023-07-24 00:07:59 +1000

    Visual updates

--- lathertech/src/App.js
+++ lathertech/src/App.js
@@ -1,13 +1,20 @@
-import NewComponent from './newComponent';
-import Rotate from './rotateArea';
+import Rotate from "./rotateArea";
 
 function App() {
   return (
-    <div className="bg-gray-900 min-h-screen h-[200vh]">
+    <div  className="bg-gray-900 min-h-screen h-[200vh]">
+      <div id="headingBanner" className="relative flex justify-center bg-green-400 h-[26vw]">
+        <div className="text-[24vw] text-white font-bebas absolute -translate-x-4 leading-none">
+          LATHERTECH
+        </div>
+        <div className="text-[24vw] text-slate-400 font-bebas absolute leading-none">
+          LATHERTECH
+        </div>
+      </div>
 
-        <NewComponent/>
-        <div className="pt-96"><Rotate/></div>
-        
+      <div className="pt-96">
+        <Rotate />
+      </div>
     </div>
   );
 }
